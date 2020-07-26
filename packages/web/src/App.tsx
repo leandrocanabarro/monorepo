2d2f4642e2d094fe81c6159d6a13c006cc4e9cc9
Leandro Canabarro <leandrocanabarro@gmail.com>
2020-07-26 18:06:01 -0300

    config jest and reuse package

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import api from '@monorepo/axios-config'
 
 const App: React.FC = () => {
+  React.useEffect(() => {
+    api.get('/').then(response => {
+      console.log(response)
+    })
+  }, [])
+
   return <h1>Hello World</h1>
 }
 
